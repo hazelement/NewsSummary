4e04f74f0f8b6c64f2c452eda271555851c1b795
Harry Zheng <harry.zheng@outlook.com>
2019-04-22 09:22:21 -0600

    added UrlDigestion DB model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class UrlDigestion(models.Model):
+
+    url_hash = models.TextField(primary_key=True)
+    url = models.TextField(default="")
+    url_digestion = models.TextField(default="")
+    update_time = models.DateTimeField(default=datetime.utcnow)
